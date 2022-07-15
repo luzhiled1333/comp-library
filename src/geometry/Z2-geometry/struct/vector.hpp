07f1d0fe8b966ca6bf2c9e2def53768dde2999f1
Luzhiled <Luzhiled+github@gmail.com>
2022-07-15 23:48:17 +0900

    new Z2::Vector

---
+++ src/geometry/Z2-geometry/struct/vector.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <vector>
+
+namespace luz {
+
+namespace Z2 {
+
+  template< typename Z >
+  class Vector {
+
+    Z x_, y_;
+
+   public:
+    Vector() {}
+    Vector(Z x, Z y): x_(x), y_(y) {}
+
+    bool operator==(const Vector &v) const {
+      return x_ == v.x_ and y_ == v.y_;
+    }
+
+    bool operator!=(const Vector &v) const {
+      return x_ == v.x_ or  y_ == v.y_;
+    }
+
+    Vector &operator+=(const Vector &v) {
+      x_ += v.x_; y_ += v.y_;
+      return *this;
+    }
+
+    Vector &operator-=(const Vector &v) {
+      x_ -= v.x_; y_ -= v.y_;
+      return *this;
+    }
+
+    Vector operator+(const Vector &v) const {
+      return (*this) += v;
+    }
+
+    Vector operator-(const Vector &v) const {
+      return (*this) -= v;
+    }
+  };
+
+  template< typename Z >
+  using Vectors = std::vector< Vector<Z> >;
+
+} // namespace Z2
+
+} // namespace luz
