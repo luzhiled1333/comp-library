a705c30b7c6e22f33b51dd4a88b0c9a9b7936712
Luzhiled <Luzhiled+github@gmail.com>
2022-07-15 23:48:58 +0900

    new Z2::Point

---
+++ src/geometry/Z2-geometry/struct/point.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "src/geometry/Z2-geometry/struct/vector.hpp"
+
+#include <vector>
+
+namespace luz {
+ 
+namespace Z2 {
+
+  template< typename Z >
+  using Point = Vector< Z >;
+
+  template< typename Z >
+  using Points = std::vector< Point<Z> >;
+
+} // namespace Z2
+
+} // namespace luz
+
