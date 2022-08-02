51552ff9e466d9560bbf91e217d30476dd3e5838
Luzhiled <Luzhiled+github@gmail.com>
2022-08-02 17:03:07 +0900

    append groups()

--- src/data-structure/disjoint-set-union.hpp
+++ src/data-structure/disjoint-set-union.hpp
@@ -60,6 +60,32 @@ namespace luz {
       return -vals_[impl_leader(v)];
     }
 
+    std::vector< std::vector< usize > > groups() {
+      std::vector< std::vector< usize > > result(n_);
+
+      std::vector< usize > leaders(n_), g_sizes(n_);
+      for (usize v: rep(0, n_)) {
+        leaders[v] = impl_leader(v);
+        g_sizes[leaders[v]]++;
+      }
+      for (usize v: rep(0, n_)) {
+        result[v].reserve(g_sizes[v]);
+      }
+      for (usize v: rep(0, n_)) {
+        result[leaders[v]].emplace_back(v);
+      }
+
+      auto empty_check = [](const std::vector< usize > &vs) {
+        return vs.empty();
+      };
+      result.erase(
+        std::remove_if(result.begin(), result.end(), empty_check),
+        result.end()
+      );
+
+      return result;
+    }
+
   };
 
 } // namespace luz
