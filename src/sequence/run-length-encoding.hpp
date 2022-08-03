f950168c8bc8d125f119f013fa2b192bc230371a
Luzhiled <Luzhiled+github@gmail.com>
2022-08-04 03:41:04 +0900

    add Run Length Encoding (#38)
    * add Run Length Encoding
    
    * add test abc259-c (no full testcase)
    
    * init docs
    
    * split luz::impl and luz
    
    * update docs

---
+++ src/sequence/run-length-encoding.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "src/cpp-template/header/type-alias.hpp"
+
+#include <vector>
+#include <string>
+#include <utility>
+
+namespace luz {
+namespace impl {
+
+  template< typename T >
+  std::vector< std::pair< T, usize > > rle(const std::vector< T > &vs) {
+    std::vector< std::pair< T, usize > > result;
+    for (const auto &v: vs) {
+      if (result.empty() or result.back().first != v) {
+        result.emplace_back(v, 0);
+      }
+      result.back().second++;
+    }
+
+    return result;
+  }
+
+} // namespace impl
+} // namespace luz
+
+namespace luz {
+
+  template< typename T >
+  std::vector< std::pair< T, usize > > run_length_encoding(const std::vector< T > &vs) {
+    return impl::rle(vs);
+  }
+  
+  std::vector< std::pair< char, usize > > run_length_encoding(const std::string &s) {
+    std::vector< char > cs(s.begin(), s.end());
+    return impl::rle(cs);
+  }
+
+} // namespace luz
---
+++ test/atcoder/abc259_c.cpp
@@ -0,0 +1,43 @@
+// verification-helper: PROBLEM https://atcoder.jp/contests/abc259/tasks/abc259_c
+
+#include "src/cpp-template/header/type-alias.hpp"
+#include "src/cpp-template/header/change-minmax.hpp"
+#include "src/cpp-template/header/rep.hpp"
+#include "src/sequence/run-length-encoding.hpp"
+
+#include <string>
+#include <iostream>
+#include <algorithm>
+
+namespace luz {
+
+  void main_() {
+    std::string s, t;
+    std::cin >> s >> t;
+
+    auto ss = run_length_encoding(s);
+    auto ts = run_length_encoding(t);
+
+    usize n = std::min(ss.size(), ts.size());
+    for (usize i: rep(0, n)) {
+      auto &[sc, scnt] = ss[i];
+      auto &[tc, tcnt] = ts[i];
+
+      if (scnt >= 2) {
+        chmin(tcnt, scnt);
+      }
+    }
+
+    if (ss == ts) {
+      std::cout << "Yes" << std::endl;
+    } else {
+      std::cout << "No" << std::endl;
+    }
+
+  }
+
+} // namespace luz
+
+int main() {
+  luz::main_();
+}
