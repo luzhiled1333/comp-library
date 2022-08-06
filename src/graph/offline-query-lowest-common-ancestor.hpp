ae5ff9be06ba9e498e70e93f183e35f7ed6ea3e8
Luzhiled <Luzhiled+github@gmail.com>
2022-08-06 20:47:41 +0900

    Update offline lowest common ancestor (#44)
    * delete inheritance
    
    * delete unused source
    
    * fix member name
    
    * add interface
    
    * fix add_query()
    
    * fix build()
    
    * fix lca()
    
    * fix CE
    
    * fix interface
    
    * [auto-verifier] verify commit 6f951df4283e2c91686b5aa859b3de4cefa0503f
    
    * simplify
    
    * fix name
    
    * fix header
    
    * assertion
    
    * fix CE
    
    * [auto-verifier] verify commit 72c79c0feef905bb2a025a7cfa74d2a111575e08
    
    Co-authored-by: GitHub <noreply@github.com>

--- src/graph/offline-query-lowest-common-ancestor.hpp
+++ src/graph/offline-query-lowest-common-ancestor.hpp
@@ -1,64 +1,81 @@
-#include <vector>
-#include <utility>
-
 #include "src/cpp-template/header/rep.hpp"
 #include "src/cpp-template/header/type-alias.hpp"
 #include "src/data-structure/disjoint-set-union.hpp"
 #include "src/graph/graph-template.hpp"
 
+#include <vector>
+#include <utility>
+#include <unordered_map>
+#include <cassert>
+
 namespace luz {
 
   template< typename cost_type >
-  class OfflineLCAQuery: Graph< cost_type > {
-    using Graph< cost_type >::g;
+  class OfflineLCAQuery {
+    usize g_size_;
+    Graph< cost_type > g_;
 
-    std::vector< std::vector< std::pair< usize, usize > > > qs;
+    usize query_count_;
+    std::vector< std::vector< std::pair< usize, usize > > > qs_;
 
-    DisjointSetUnion dsu;
-    std::vector< bool > visited;
-    std::vector< usize > ancestors;
+    std::vector< std::unordered_map< usize, usize > > to_qi_;
 
-    void dfs(usize v, std::vector< usize > &ret) {
-      visited[v] = true;
-      ancestors[v] = v;
+    DisjointSetUnion dsu_;
+    std::vector< bool > visited_;
+    std::vector< usize > ancestors_;
 
-      for (const auto &e: g[v]) {
-        if (visited[e.to]) continue;
-        dfs(e.to, ret);
-        dsu.merge(v, e.to);
-        ancestors[dsu.leader(v)] = v;
+    std::vector< usize > query_results_;
+
+    void bound_check(usize v) const {
+      assert(v < g_size_);
+    }
+
+    void dfs(usize v) {
+      visited_[v] = true;
+      ancestors_[v] = v;
+
+      for (const auto &e: g_[v]) {
+        if (visited_[e.to]) continue;
+        dfs(e.to);
+        dsu_.merge(v, e.to);
+        ancestors_[dsu_.leader(v)] = v;
       }
 
-      for (const auto &[u, qi]: qs[v]) {
-        if (not visited[u]) continue;
-        ret[qi] = ancestors[dsu.leader(u)];
+      for (const auto &[u, qi]: qs_[v]) {
+        if (not visited_[u]) continue;
+        query_results_[qi] = ancestors_[dsu_.leader(u)];
       }
     }
 
    public:
     using Queries = std::vector< std::pair< usize, usize > >;
 
-    using Graph< cost_type >::Graph;
-    using Graph< cost_type >::add_undirected_edge;
-    using Graph< cost_type >::size;
+    OfflineLCAQuery(Graph< cost_type > &g):
+      g_size_(g.size()), g_(g),
+      query_count_(0), qs_(g_size_), to_qi_(g_size_),
+      dsu_(g_size_), visited_(g_size_, false), ancestors_(g_size_) {}
 
-    std::vector< usize > solve(const Queries &queries, usize root) {
-      usize q = queries.size();
-      qs.resize(size());
-      for (usize qi: rep(0, q)) {
-        const auto [u, v] = queries[qi];
-        qs[u].emplace_back(v, qi);
-        qs[v].emplace_back(u, qi);
-      }
+    usize add_query(usize u, usize v) {
+      bound_check(u);
+      bound_check(v);
+      qs_[u].emplace_back(v, query_count_);
+      qs_[v].emplace_back(u, query_count_);
+      to_qi_[u][v] = to_qi_[v][u] = query_count_;
+      return query_count_++;
+    }
 
-      dsu = DisjointSetUnion(size());
-      visited.assign(size(), false);
-      ancestors.resize(size());
+    void build(usize root) {
+      bound_check(root);
+      query_results_.resize(query_count_);
+      dfs(root);
+    }
 
-      std::vector< usize > ret(q);
-      dfs(root, ret);
-      qs.clear();
-      return ret;
+    usize lca(usize u, usize v) {
+      bound_check(u);
+      bound_check(v);
+      assert(to_qi_[u].count(v));
+      usize qi = to_qi_[u][v];
+      return query_results_[qi];
     }
 
   };
--- test/aoj/grl-5-c.test.cpp
+++ test/aoj/grl-5-c.test.cpp
@@ -2,6 +2,7 @@
 
 #include "src/cpp-template/header/type-alias.hpp"
 #include "src/cpp-template/header/rep.hpp"
+#include "src/graph/graph-template.hpp"
 #include "src/graph/offline-query-lowest-common-ancestor.hpp"
 
 #include <iostream>
@@ -12,7 +13,7 @@ namespace luz {
     usize n;
     std::cin >> n;
 
-    OfflineLCAQuery< i32 > g(n);
+    Graph< i32 > g(n);
     for (usize v: rep(0, n)) {
       usize k;
       std::cin >> k;
@@ -24,17 +25,20 @@ namespace luz {
       }
     }
 
+    OfflineLCAQuery offline_lcas(g);
+
     usize q;
     std::cin >> q;
 
-    OfflineLCAQuery< i32 >::Queries qs(q);
+    std::vector< std::pair< usize, usize > > qs(q);
     for (auto &[u, v]: qs) {
       std::cin >> u >> v;
+      offline_lcas.add_query(u, v);
     }
 
-    auto ans = g.solve(qs, 0);
-    for (auto &v: ans) {
-      std::cout << v << std::endl;
+    offline_lcas.build(0);
+    for (const auto &[u, v]: qs) {
+      std::cout << offline_lcas.lca(u, v) << std::endl;
     }
 
   }
