e6638aa48dd5ee297645524cc130b4746a472a43
Luzhiled <Luzhiled+github@gmail.com>
2022-08-06 18:16:00 +0900

    fix member name

--- src/graph/offline-query-lowest-common-ancestor.hpp
+++ src/graph/offline-query-lowest-common-ancestor.hpp
@@ -11,52 +11,52 @@ namespace luz {
   template< typename cost_type >
   class OfflineLCAQuery {
     usize g_size_;
-    Graph< cost_type > g;
+    Graph< cost_type > g_;
 
-    std::vector< std::vector< std::pair< usize, usize > > > qs;
+    std::vector< std::vector< std::pair< usize, usize > > > qs_;
 
-    DisjointSetUnion dsu;
-    std::vector< bool > visited;
-    std::vector< usize > ancestors;
+    DisjointSetUnion dsu_;
+    std::vector< bool > visited_;
+    std::vector< usize > ancestors_;
 
     void dfs(usize v, std::vector< usize > &ret) {
-      visited[v] = true;
-      ancestors[v] = v;
+      visited_[v] = true;
+      ancestors_[v] = v;
 
-      for (const auto &e: g[v]) {
-        if (visited[e.to]) continue;
+      for (const auto &e: g_[v]) {
+        if (visited_[e.to]) continue;
         dfs(e.to, ret);
-        dsu.merge(v, e.to);
-        ancestors[dsu.leader(v)] = v;
+        dsu_.merge(v, e.to);
+        ancestors_[dsu_.leader(v)] = v;
       }
 
-      for (const auto &[u, qi]: qs[v]) {
-        if (not visited[u]) continue;
-        ret[qi] = ancestors[dsu.leader(u)];
+      for (const auto &[u, qi]: qs_[v]) {
+        if (not visited_[u]) continue;
+        ret[qi] = ancestors_[dsu_.leader(u)];
       }
     }
 
    public:
     using Queries = std::vector< std::pair< usize, usize > >;
 
-    OfflineLCAQuery(Graph< cost_type > &g_): g_size_(g_.size()), g(g_) {}
+    OfflineLCAQuery(Graph< cost_type > &g): g_size_(g.size()), g_(g) {}
 
     std::vector< usize > solve(const Queries &queries, usize root) {
       usize q = queries.size();
-      qs.resize(g_size_);
+      qs_.resize(g_size_);
       for (usize qi: rep(0, q)) {
         const auto [u, v] = queries[qi];
-        qs[u].emplace_back(v, qi);
-        qs[v].emplace_back(u, qi);
+        qs_[u].emplace_back(v, qi);
+        qs_[v].emplace_back(u, qi);
       }
 
-      dsu = DisjointSetUnion(g_size_);
-      visited.assign(g_size_, false);
-      ancestors.resize(g_size_);
+      dsu_ = DisjointSetUnion(g_size_);
+      visited_.assign(g_size_, false);
+      ancestors_.resize(g_size_);
 
       std::vector< usize > ret(q);
       dfs(root, ret);
-      qs.clear();
+      qs_.clear();
       return ret;
     }
 
