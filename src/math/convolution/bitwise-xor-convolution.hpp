7da020316058a559a011895103e5c89dc3ee8930
ei1333 <ei1333hobby@gmail.com>
2022-08-09 20:55:19 +0900

    uku

--- src/math/convolution/bitwise-xor-convolution.hpp
+++ src/math/convolution/bitwise-xor-convolution.hpp
@@ -13,12 +13,26 @@ namespace luz {
   std::vector< T > bitwise_xor_convolution(std::vector< T > f, std::vector< T > g) {
     assert(f.size() == g.size());
 
-    fast_walsh_hadamard_transform(f);
-    fast_walsh_hadamard_transform(g);
+    T inv2 = T(1) / T(2);
+    auto zeta = [](T& lo, T& hi) {
+      T x = lo + hi;
+      T y = lo - hi;
+      lo = x;
+      hi = y;
+    };
+    auto mobius = [inv2](T& lo, T& hi) {
+      T x = lo + hi;
+      T y = lo - hi;
+      lo = x * inv2;
+      hi = y * inv2;
+    };
+
+    fast_walsh_hadamard_transform(f, zeta);
+    fast_walsh_hadamard_transform(g, zeta);
     for (usize i: rep(0, f.size())) {
       f[i] *= g[i];
     }
-    fast_walsh_hadamard_inverse_transform(f);
+    fast_walsh_hadamard_transform(f, mobius);
     return f;
   }
 
