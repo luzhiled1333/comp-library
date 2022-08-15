ac740dc2825d484e095a494f4d9a9e484f0be163
Luzhiled <Luzhiled+github@gmail.com>
2022-08-15 22:45:30 +0900

    draft: pi/eps

---
+++ src/geometry/R2/numbers/pi.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <cmath>
+
+namespace luz {
+namespace R2 {
+
+  long double pi() {
+    static long double PI = acosl(-1);
+    return PI;
+  }
+
+} // namespace R2
+} // namespace luz
