b8537af08697d8cce7664a2b2b32934a0044445a
Luzhiled <Luzhiled+github@gmail.com>
2022-08-15 21:35:32 +0900

    new point

---
+++ src/geometry/R2/class/point.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "src/geometry/R2/class/vector.hpp"
+
+#include <vector>
+
+namespace luz {
+namespace R2 {
+
+  template< typename Z >
+  using Point = Vector< Z >;
+
+  template< typename Z >
+  using Points = std::vector< Point<Z> >;
+
+} // namespace R2
+} // namespace luz
+
