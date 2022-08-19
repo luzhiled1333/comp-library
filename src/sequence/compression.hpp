4dfbc6bff824458043521bda1f2f508c3286055c
Luzhiled <Luzhiled+github@gmail.com>
2022-08-19 16:42:24 +0900

    delete coordinate_compression()

--- src/sequence/compression.hpp
+++ src/sequence/compression.hpp
@@ -37,19 +37,4 @@ namespace luz {
     }
   };
 
-  template< typename T >
-  std::vector< usize > coordinate_compression(std::vector< T > vs) {
-    std::vector< usize > res(vs.size());
-
-    std::vector< T > zip = vs;
-    std::sort(zip.begin(), zip.end());
-    zip.erase(std::unique(zip.begin(), zip.end()), zip.end());
-
-    for (usize i: rep(0, vs.size())) {
-      res[i] = std::lower_bound(zip.begin(), zip.end(), vs[i]) - zip.begin();
-    }
-
-    return res;
-  }
-
 }
