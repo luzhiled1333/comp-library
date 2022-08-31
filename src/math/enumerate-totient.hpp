cef584bb1a233514b88df64e388079d9b91dbdbd
Luzhiled <Luzhiled+github@gmail.com>
2022-08-31 13:50:41 +0900

    fix type

--- src/math/enumerate-totient.hpp
+++ src/math/enumerate-totient.hpp
@@ -14,12 +14,12 @@ namespace luz {
                   "T must be integer");
     assert(n >= 0);
     std::vector< T > totient(n);
-    for (u32 i: rep(0, n)) {
+    for (T i: rep(0, n)) {
       totient[i] = i;
     }
-    for (u32 i: rep(2, n)) {
+    for (T i: rep(2, n)) {
       if (totient[i] == i) {
-        for (u32 j = i; j < n; j += i) {
+        for (T j = i; j < n; j += i) {
           totient[j] = totient[j] / i * (i - 1);
         }
       }
