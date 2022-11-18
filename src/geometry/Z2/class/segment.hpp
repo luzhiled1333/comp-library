9aaecf2ac2805f3ba0a8717e49c8d3ad62b9767f
Luzhiled <Luzhiled+github@gmail.com>
2022-11-18 15:49:20 +0900

    add segment

---
+++ src/geometry/Z2/class/segment.hpp
@@ -0,0 +1,32 @@
+#include <cassert>
+#include <vector>
+
+#include "src/geometry/Z2/class/point.hpp"
+
+namespace luz::Z2 {
+
+  template< typename Z >
+  class Segment {
+    Point<Z> p0_, p1_;
+
+   public:
+    Segment() = default;
+
+    Segment(Point<Z> p0, Point<Z> p1): p0_(p0), p1_(p1) {
+      assert(p0 != p1);
+    }
+
+    Point<Z> p0() const {
+      return p0_;
+    }
+
+    Point<Z> p1() const {
+      return p1_;
+    }
+
+  };
+
+  template< typename Z >
+  using Segments = std::vector< Segment<Z> >;
+
+} // namespace luz::Z2
