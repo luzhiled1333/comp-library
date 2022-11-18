cb0de01998f5162dc041ab0ff6716b8e7485d7f3
Luzhiled <Luzhiled+github@gmail.com>
2022-11-18 15:43:40 +0900

    add line

---
+++ src/geometry/Z2/class/line.hpp
@@ -0,0 +1,32 @@
+#include <cassert>
+#include <vector>
+
+#include "src/geometry/Z2/class/point.hpp"
+
+namespace luz::Z2 {
+
+  template< typename Z >
+  class Line {
+    Point<Z> p0_, p1_;
+
+   public:
+    Line() = default;
+
+    Line(Point<Z> p0, Point<Z> p1): p0_(p0), p1_(p1) {
+      assert(p0 != p1);
+    }
+
+    Point<Z> p0() const {
+      return p0_;
+    }
+
+    Point<Z> p1() const {
+      return p1_;
+    }
+
+  };
+
+  template< typename Z >
+  using Lines = std::vector< Line<Z> >;
+
+} // namespace luz::Z2
