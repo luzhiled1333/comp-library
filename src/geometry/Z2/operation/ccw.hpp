f5f4daab5dcc42567e377aad60037a120b1e1b9b
Luzhiled <Luzhiled+github@gmail.com>
2022-11-18 15:38:39 +0900

    add Z2::ccw

---
+++ src/geometry/Z2/operation/ccw.hpp
@@ -0,0 +1,34 @@
+#include "src/cpp-template/header/type-alias.hpp"
+#include "src/geometry/Z2/constants/ccw-constants.hpp"
+#include "src/geometry/Z2/operation/square-norm.hpp"
+#include "src/geometry/Z2/operation/cross-product.hpp"
+#include "src/geometry/Z2/operation/inner-product.hpp"
+#include "src/geometry/Z2/class/vector.hpp"
+
+namespace luz::Z2::impl_ccw {
+  
+  using namespace constants::ccw;
+
+  template< typename Z >
+  i32 ccw(const Vector<Z> &a, Vector<Z> b, Vector<Z> c) {
+    b -= a;
+    c -= a;
+
+    Z cp = cross_product(b, c);
+    if (cp > 0) return COUNTER_CLOCKWISE;
+    if (cp < 0) return CLOCKWISE;
+    if (inner_product(b, c) < 0) return ONLINE_BACK;
+    if (square_norm(b) < square_norm(c)) return ONLINE_FRONT;
+    return ON_SEGMENT;
+  }
+
+}
+
+namespace luz::Z2 {
+
+  template< typename Z >
+  i32 ccw(const Vector<Z> &a, const Vector<Z> &b, const Vector<Z> &c) {
+    return impl_ccw::ccw(a, b, c);
+  }
+
+}
