d51fd86bdd7b51ece53708549faa0522c0d25ff7
Luzhiled <Luzhiled+github@gmail.com>
2022-11-18 15:19:42 +0900

    add Z2::inner_product

---
+++ src/geometry/Z2/operation/inner_product.hpp
@@ -0,0 +1,10 @@
+#include "src/geometry/Z2/class/vector.hpp"
+
+namespace luz::Z2 {
+
+  template< typename Z >
+  Z inner_product(const Vector<Z> &a, const Vector<Z> &b) {
+    return a.x() * b.x() + a.y() * b.y();
+  }
+
+}
