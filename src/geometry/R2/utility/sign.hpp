1e7600814f3a124262ebf9bccd0cdd2e8f94ac53
Luzhiled <Luzhiled+github@gmail.com>
2023-02-02 13:05:58 +0900

    fix sign()

--- src/geometry/R2/utility/sign.hpp
+++ src/geometry/R2/utility/sign.hpp
@@ -1,5 +1,17 @@
 #pragma once
 
-namespace luz {
-  namespace R2 {} // namespace R2
-} // namespace luz
+#include "src/cpp-template/header/type-alias.hpp"
+#include "src/geometry/R2/numbers/eps.hpp"
+#include "src/geometry/R2/numbers/sign.hpp"
+
+namespace luz::R2 {
+
+  template < typename R >
+  i32 sign(R r) {
+    set_eps(1e-10);
+    if (r < -eps()) return numbers::sign::negative;
+    if (r > +eps()) return numbers::sign::positive;
+    return numbers::sign::zero;
+  }
+
+} // namespace luz::R2
