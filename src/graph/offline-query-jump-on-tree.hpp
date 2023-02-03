d5540624668a1412b956ef966998a305596374b2
Luzhiled <Luzhiled+github@gmail.com>
2023-02-03 21:47:16 +0900

    wip

--- src/graph/offline-query-jump-on-tree.hpp
+++ src/graph/offline-query-jump-on-tree.hpp
@@ -1,27 +1,50 @@
 #pragma once
 
 #include "src/cpp-template/header/type-alias.hpp"
+#include "src/graph/distances_on_undirected_graph.hpp"
 #include "src/graph/graph-template.hpp"
+#include "src/graph/offline-query-level-ancestor.hpp"
+#include "src/graph/offline-query-lowest-common-ancestor.hpp"
 
 #include <vector>
 
 namespace luz {
 
-  template< typename cost_type >
+  template < typename cost_type >
   class OfflineJumpOnTreeQuery {
     usize g_size_;
     Graph< cost_type > g_;
+    OfflineLCAQuery< cost_type > lca_;
+    OfflineLAQuery< cost_type > la_;
 
     usize query_count_;
+    std::vector< std::tuple< usize, usize, usize > > qs_;
+
+    std::vector< std::pair< usize, usize > > converted_qs_;
 
    public:
-    OfflineJumpOnTreeQuery(Graph< cost_type > &g) : g_(g) {}
+    explicit OfflineJumpOnTreeQuery(Graph< cost_type > &g)
+      : g_size_(g.size()),
+        g_(g),
+        lca_(g),
+        la_(g),
+        query_count_(0) {}
 
-    usize add_query(usize begin, usize end, usize distance) {}
+    usize add_query(usize start, usize end, usize distance) {
+      qs_.emplace_back(start, end, distance);
+    }
 
-    void build(usize root) {}
+    void build(usize root) {
+      for (auto &[s, t, _]: qs_) {
+        lca_.add_query(s, t);
+      }
 
-    std::optional< usize > jump_on_tree(usize begin, usize end, usize distance) {}
+      lca_.build(root);
+      std::vector< usize > depths = distance_on_undirected_graph(g_, root);
+    }
+
+    std::optional< usize > jump_on_tree(usize start, usize end,
+                                        usize distance) {}
   };
 
-}
+} // namespace luz
