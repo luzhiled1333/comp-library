85541affb6e6b5b29802117d6b782cdb8920af11
Luzhiled <Luzhiled+github@gmail.com>
2023-02-03 18:58:53 +0900

    add distances on undirected graph

---
+++ src/graph/distances_on_undirected_graph.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "src/cpp-template/header/type-alias.hpp"
+#include "src/graph/graph-template.hpp"
+
+#include <limits>
+#include <queue>
+#include <vector>
+
+namespace luz {
+
+  template < typename cost_type >
+  std::vector< usize > distances_on_undirected_graph(
+      const Graph< cost_type > &g, usize root) {
+    cost_type not_reached = std::numeric_limits< cost_type >::max();
+    std::vector< usize > distances(g.size(), not_reached);
+    distances[root] = 0;
+
+    std::queue< usize > que;
+    que.emplace(root);
+
+    while (not que.empty()) {
+      usize v = que.front();
+      que.pop();
+
+      for (const auto &e: g[v]) {
+        usize u = e.to;
+
+        if (distances[u] != not_reached) continue;
+
+        distances[u] = distances[v] + 1;
+        que.emplace(u);
+      }
+    }
+
+    return distances;
+  }
+
+} // namespace luz
