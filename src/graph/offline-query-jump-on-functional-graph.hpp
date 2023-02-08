76ddece0626bbe224d4ffb11f2cfff4947a5fa22
ei1333 <ei1333hobby@gmail.com>
2023-02-08 21:58:10 +0900

    refactor

--- src/graph/offline-query-jump-on-functional-graph.hpp
+++ src/graph/offline-query-jump-on-functional-graph.hpp
@@ -1,61 +1,55 @@
-#pragma once
-
-#include "src/cpp-template/header/rep.hpp"
-#include "src/cpp-template/header/type-alias.hpp"
-#include "src/graph/graph-template.hpp"
-#include "src/graph/offline-query-level-ancestor.hpp"
-#include "src/utility/pair-hash.hpp"
-
-#include <unordered_map>
-#include <utility>
-#include <vector>
-
 namespace luz {
   template < typename cost_type >
   class OfflineJumpOnFunctionalGraphQuery {
-    usize g_size_;
-    Graph< cost_type > g_;
-    Graph< usize > tree_;
-    std::vector< usize > tree_depth_, tree_root_;
+    usize g_size;
+    Graph< cost_type > g;
+    Graph< usize > tree;
+    std::vector< usize > tree_depth, tree_root;
+    OfflineLAQuery< usize > la;
+    std::vector< usize > loop_id, loop_size, loop_pos;
+    std::vector< std::vector< usize > > loops;
     using query_type = std::pair< usize, u64 >;
-    std::vector< query_type > qs_;
-    std::unordered_map< query_type, usize, PairHash > results_;
+    std::vector< query_type > qs;
+    std::unordered_map< query_type, usize, PairHash > result;
 
     void dfs_on_tree(usize v, usize p) {
-      for (auto &e: tree_[v]) {
+      for (auto &e: tree[v]) {
         usize u = e.to;
         if (u == p) continue;
-        tree_root_[u]  = tree_root_[v];
-        tree_depth_[u] = tree_depth_[v] + 1;
+        tree_root[u]  = tree_root[v];
+        tree_depth[u] = tree_depth[v] + 1;
         dfs_on_tree(u, v);
       }
     }
 
    public:
     explicit OfflineJumpOnFunctionalGraphQuery(
-        const Graph< cost_type > &g)
-        : g_size_(g.size()),
-          tree_(g.size() + 1),
-          tree_depth_(g.size()),
-          tree_root_(g.size()),
-          g_(g) {
-      for (usize v: rep(0, g_size_)) {
-        assert(g_[v].size() == 1);
+        const Graph< cost_type > &g_)
+        : g_size(g_.size()),
+          tree(g_.size() + 1),
+          tree_depth(g_.size()),
+          tree_root(g_.size()),
+          loop_id(g_.size()),
+          loop_size(g_.size()),
+          loop_pos(g_.size()),
+          g(g_) {
+      for (usize v: rep(0, g_size)) {
+        assert(g[v].size() == 1);
       }
     }
 
     void add_query(usize v, u64 k) {
-      qs_.emplace_back(v, k);
+      qs.emplace_back(v, k);
     }
 
     void build() {
-      std::vector< usize > degs(g_size_);
-      for (usize v: rep(0, g_size_)) {
-        degs[g_[v][0].to]++;
+      std::vector< usize > degs(g_size);
+      for (usize v: rep(0, g_size)) {
+        degs[g[v][0].to]++;
       }
       std::vector< usize > que;
-      que.reserve(g_size_);
-      for (usize v: rep(0, g_size_)) {
+      que.reserve(g_size);
+      for (usize v: rep(0, g_size)) {
         if (degs[v] == 0) {
           que.emplace_back(v);
         }
@@ -63,24 +57,21 @@ namespace luz {
       while (not que.empty()) {
         usize v = que.back();
         que.pop_back();
-        usize u = g_[v][0].to;
+        usize u = g[v][0].to;
         degs[u]--;
-        tree_.add_undirected_edge(u, v);
+        tree.add_undirected_edge(u, v);
         if (degs[u] == 0) {
           que.emplace_back(u);
         }
       }
-      for (usize v: rep(0, g_size_)) {
+      for (usize v: rep(0, g_size)) {
         if (degs[v] != 0) {
-          tree_root_[v] = v;
-          tree_.add_undirected_edge(g_size_, v);
-          dfs_on_tree(v, g_size_);
+          tree_root[v] = v;
+          tree.add_undirected_edge(g_size, v);
+          dfs_on_tree(v, g_size);
         }
       }
-      std::vector< usize > loop_id(g_size_), loop_size(g_size_),
-          loop_pos(g_size_);
-      std::vector< std::vector< usize > > loops;
-      for (usize v: rep(0, g_size_)) {
+      for (usize v: rep(0, g_size)) {
         if (degs[v] != 0) {
           usize cur = v;
           std::vector< usize > loop;
@@ -89,43 +80,43 @@ namespace luz {
             loop_pos[cur] = loop.size();
             loop.emplace_back(cur);
             degs[cur] = 0;
-            cur       = g_[cur][0].to;
+            cur       = g[cur][0].to;
           } while (cur != v);
           do {
             loop_size[cur] = loop.size();
-            cur            = g_[cur][0].to;
+            cur            = g[cur][0].to;
           } while (cur != v);
           loops.emplace_back(std::move(loop));
         }
       }
-      OfflineLAQuery< usize > la(tree_);
-      results_.reserve(qs_.size());
-      for (auto [v, k]: qs_) {
-        if (k < tree_depth_[v]) {
-          la.add_query(v, tree_depth_[v] - k + 1);
+      la = OfflineLAQuery(tree);
+      result.reserve(qs.size());
+      for (auto [v, k]: qs) {
+        if (k < tree_depth[v]) {
+          la.add_query(v, tree_depth[v] - k + 1);
         } else {
           query_type qi(v, k);
-          k -= tree_depth_[v];
-          usize root = tree_root_[v];
+          k -= tree_depth[v];
+          usize root = tree_root[v];
           auto &loop = loops[loop_id[root]];
           k += loop_pos[root];
           k %= loop_size[root];
-          results_[qi] = loop[k];
+          result[qi] = loop[k];
         }
       }
-      la.build(g_size_);
-      for (auto [v, k]: qs_) {
-        if (k < tree_depth_[v]) {
+      la.build(g_size);
+      for (auto [v, k]: qs) {
+        if (k < tree_depth[v]) {
           query_type qi(v, k);
-          results_[qi] = la.la(v, tree_depth_[v] - k + 1).value();
+          result[qi] = la.la(v, tree_depth[v] - k + 1).value();
         }
       }
     }
 
     usize jump(usize v, u64 k) const {
       query_type qi(v, k);
-      assert(results_.count(qi));
-      return results_.find(qi)->second;
+      assert(result.count(qi));
+      return result.find(qi)->second;
     }
   };
 } // namespace luz
