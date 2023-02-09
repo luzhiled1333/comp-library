f42de2021abad137effa2e1d47e63286d2cb0d3c
Luzhiled <Luzhiled+github@gmail.com>
2023-02-10 02:41:47 +0900

    add countr_zero

---
+++ src/utility/bit/count-trailing-0s.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "src/utility/bit/popcount.hpp"
+
+#include <cassert>
+
+namespace luz {
+
+  usize countr_zero(u64 x) {
+    assert(__cplusplus <= 201703L);
+
+    if (x == 0) {
+      return 64;
+    }
+
+#ifdef __GNUC__
+    return __builtin_ctzll(x);
+#endif
+
+    return popcount((x & -x) - 1);
+  }
+
+} // namespace luz
