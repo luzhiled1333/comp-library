6be2798c9c21e1e64760291681050a705da74498
ei1333 <ei1333hobby@gmail.com>
2023-02-14 00:39:10 +0900

    add spfa

---
+++ src/graph/single-source-shortest-path/in-weighted-graph.hpp
@@ -0,0 +1,117 @@
+#pragma once
+
+#include "src/cpp-template/header/type-alias.hpp"
+#include "src/graph/graph-template.hpp"
+
+#include <limits>
+#include <queue>
+#include <vector>
+
+namespace luz::sssp {
+
+  template < typename cost_type >
+  class InWeightedGraph {
+    using graph = Graph< cost_type >;
+
+    graph g;
+    usize g_size;
+    usize source;
+    bool detected_negative_cycle;
+
+    std::vector< cost_type > ds;
+    std::vector< usize > parents, ids;
+
+    void spfa(usize s) {
+      std::queue< usize > que;
+      std::vector< usize > ds_update_cnt(g_size);
+      std::vector< bool > in_que(g_size);
+
+      ds[s]            = 0;
+      in_que[s]        = true;
+      ds_update_cnt[s] = 1;
+      que.emplace(s);
+
+      while (not que.empty()) {
+        usize v = que.front();
+        que.pop();
+        in_que[v] = false;
+
+        for (const auto &e: g[v]) {
+          usize u        = e.to;
+          cost_type cost = e.cost;
+          if (ds[v] + cost >= ds[u]) {
+            continue;
+          }
+          ds[u]      = ds[v] + cost;
+          parents[u] = v;
+          ids[u]     = e.id;
+          if (in_que[u]) {
+            continue;
+          }
+          in_que[u] = true;
+          ds_update_cnt[u]++;
+          if (ds_update_cnt[u] >= g_size) {
+            detected_negative_cycle = true;
+            return;
+          }
+          que.emplace(u);
+        }
+      }
+    }
+
+   public:
+    explicit InWeightedGraph(const graph &g_, usize source_)
+        : g(g_),
+          g_size(g.size()),
+          source(source_),
+          detected_negative_cycle(false),
+          ds(g_size, inf()),
+          parents(g_size, undefined()),
+          ids(g_size, undefined()) {
+      spfa(source);
+    }
+
+    graph get_original_graph() const {
+      return g;
+    }
+
+    static inline cost_type inf() {
+      static cost_type inf_ = std::numeric_limits< cost_type >::max();
+      return inf_;
+    }
+
+    inline cost_type distance(const usize v) const {
+      return ds[v];
+    }
+
+    inline std::vector< cost_type > get_distances() const {
+      return ds;
+    }
+
+    static inline usize undefined() {
+      static usize undefined_ = std::numeric_limits< usize >::max();
+      return undefined_;
+    }
+
+    inline usize parent(const usize v) const {
+      return parents[v];
+    }
+
+    inline std::vector< usize > get_parent() const {
+      return parents;
+    }
+
+    inline usize edge_label(const usize v) const {
+      return ids[v];
+    }
+
+    inline std::vector< usize > get_edge_labels() const {
+      return ids;
+    }
+
+    inline bool is_negative_cycle() const {
+      return detected_negative_cycle;
+    }
+  };
+
+} // namespace luz::sssp
