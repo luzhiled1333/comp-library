66f4ea46fb085acc2c411a84965a6c9f171f7e3c
ei1333 <ei1333hobby@gmail.com>
2023-03-09 03:02:26 +0900

    mint -> modint

--- src/math/convolution/modint-convolution.hpp
+++ src/math/convolution/modint-convolution.hpp
@@ -9,7 +9,7 @@
 
 namespace luz::internal {
 
-  template < class mint >
+  template < class modint >
   class ButterflyInfo {
    public:
     ButterflyInfo() {
@@ -17,7 +17,7 @@ namespace luz::internal {
         return;
       }
 
-      u32 mod = mint::get_mod();
+      u32 mod = modint::get_mod();
       assert(mod >= 3 && mod % 2 == 1);
       u32 tmp        = mod - 1;
       usize max_base = 0;
@@ -25,11 +25,11 @@ namespace luz::internal {
         tmp >>= 1;
         max_base++;
       }
-      mint root = 2;
-      while (root.pow((mod - 1) >> 1) == mint(1)) {
+      modint root = 2;
+      while (root.pow((mod - 1) >> 1) == modint(1)) {
         root += 1;
       }
-      assert(root.pow(mod - 1) == mint(1));
+      assert(root.pow(mod - 1) == modint(1));
       assert(max_base >= 2);
       roots.resize(max_base + 1);
       iroots.resize(max_base + 1);
@@ -37,12 +37,12 @@ namespace luz::internal {
       irate3.resize(max_base + 1);
 
       roots[max_base]  = root.pow((mod - 1) >> max_base);
-      iroots[max_base] = mint(1) / roots[max_base];
+      iroots[max_base] = modint(1) / roots[max_base];
       for (usize i: rrep(0, max_base)) {
         roots[i]  = roots[i + 1] * roots[i + 1];
         iroots[i] = iroots[i + 1] * iroots[i + 1];
       }
-      mint prod = 1, iprod = 1;
+      modint prod = 1, iprod = 1;
       for (usize i: rep(0, max_base - 2)) {
         rate3[i]  = roots[i + 3] * prod;
         irate3[i] = iroots[i + 3] * iprod;
@@ -50,7 +50,7 @@ namespace luz::internal {
         iprod *= roots[i + 3];
       }
     }
-    static std::vector< mint > roots, iroots, rate3, irate3;
+    static std::vector< modint > roots, iroots, rate3, irate3;
   };
   template < typename mint >
   std::vector< mint > ButterflyInfo< mint >::roots =
@@ -65,19 +65,19 @@ namespace luz::internal {
   std::vector< mint > ButterflyInfo< mint >::irate3 =
       std::vector< mint >();
 
-  template < typename mint >
-  void butterfly(std::vector< mint > &vs) {
+  template < typename modint >
+  void butterfly(std::vector< modint > &vs) {
     usize n = vs.size();
     assert((n & (n - 1)) == 0);
     usize h = bit_width(n) - 1;
-    ButterflyInfo< mint > info;
+    ButterflyInfo< modint > info;
     assert(h < info.iroots.size());
-    usize len = 0;
-    mint imag = info.roots[2];
+    usize len   = 0;
+    modint imag = info.roots[2];
     if (h & 1) {
       usize p = 1 << (h - 1);
       for (usize i: rep(0, p)) {
-        mint r    = vs[i + p];
+        modint r  = vs[i + p];
         vs[i + p] = vs[i] - r;
         vs[i] += r;
       }
@@ -86,33 +86,33 @@ namespace luz::internal {
     while (len + 1 < h) {
       usize p = 1 << (h - len - 2);
       for (usize i: rep(0, p)) {
-        mint a0        = vs[i + 0 * p];
-        mint a1        = vs[i + 1 * p];
-        mint a2        = vs[i + 2 * p];
-        mint a3        = vs[i + 3 * p];
-        mint a1na3imag = (a1 - a3) * imag;
-        mint a0a2      = a0 + a2;
-        mint a1a3      = a1 + a3;
-        mint a0na2     = a0 - a2;
-        vs[i + 0 * p]  = a0a2 + a1a3;
-        vs[i + 1 * p]  = a0a2 - a1a3;
-        vs[i + 2 * p]  = a0na2 + a1na3imag;
-        vs[i + 3 * p]  = a0na2 - a1na3imag;
+        modint a0        = vs[i + 0 * p];
+        modint a1        = vs[i + 1 * p];
+        modint a2        = vs[i + 2 * p];
+        modint a3        = vs[i + 3 * p];
+        modint a1na3imag = (a1 - a3) * imag;
+        modint a0a2      = a0 + a2;
+        modint a1a3      = a1 + a3;
+        modint a0na2     = a0 - a2;
+        vs[i + 0 * p]    = a0a2 + a1a3;
+        vs[i + 1 * p]    = a0a2 - a1a3;
+        vs[i + 2 * p]    = a0na2 + a1na3imag;
+        vs[i + 3 * p]    = a0na2 - a1na3imag;
       }
-      mint rot = info.rate3[0];
+      modint rot = info.rate3[0];
       for (usize s: rep(1, 1 << len)) {
         usize offset = s << (h - len);
-        mint rot2    = rot * rot;
-        mint rot3    = rot2 * rot;
+        modint rot2  = rot * rot;
+        modint rot3  = rot2 * rot;
         for (usize i: rep(0, p)) {
-          mint a0                = vs[i + offset + 0 * p];
-          mint a1                = vs[i + offset + 1 * p] * rot;
-          mint a2                = vs[i + offset + 2 * p] * rot2;
-          mint a3                = vs[i + offset + 3 * p] * rot3;
-          mint a1na3imag         = (a1 - a3) * imag;
-          mint a0a2              = a0 + a2;
-          mint a1a3              = a1 + a3;
-          mint a0na2             = a0 - a2;
+          modint a0              = vs[i + offset + 0 * p];
+          modint a1              = vs[i + offset + 1 * p] * rot;
+          modint a2              = vs[i + offset + 2 * p] * rot2;
+          modint a3              = vs[i + offset + 3 * p] * rot3;
+          modint a1na3imag       = (a1 - a3) * imag;
+          modint a0a2            = a0 + a2;
+          modint a1a3            = a1 + a3;
+          modint a0na2           = a0 - a2;
           vs[i + offset + 0 * p] = a0a2 + a1a3;
           vs[i + offset + 1 * p] = a0a2 - a1a3;
           vs[i + offset + 2 * p] = a0na2 + a1na3imag;
@@ -124,45 +124,45 @@ namespace luz::internal {
     }
   }
 
-  template < typename mint >
-  void butterfly_inv(std::vector< mint > &vs) {
+  template < typename modint >
+  void butterfly_inv(std::vector< modint > &vs) {
     usize n = vs.size();
     assert((n & (n - 1)) == 0);
     usize h = bit_width(n) - 1;
-    ButterflyInfo< mint > info;
+    ButterflyInfo< modint > info;
     assert(h < info.iroots.size());
-    usize len  = h;
-    mint iimag = info.iroots[2];
+    usize len    = h;
+    modint iimag = info.iroots[2];
     while (len > 1) {
       usize p = 1 << (h - len);
       for (usize i: rep(0, p)) {
-        mint a0         = vs[i + 0 * p];
-        mint a1         = vs[i + 1 * p];
-        mint a2         = vs[i + 2 * p];
-        mint a3         = vs[i + 3 * p];
-        mint a2na3iimag = (a2 - a3) * iimag;
-        mint a0na1      = a0 - a1;
-        mint a0a1       = a0 + a1;
-        mint a2a3       = a2 + a3;
-        vs[i + 0 * p]   = a0a1 + a2a3;
-        vs[i + 1 * p]   = (a0na1 + a2na3iimag);
-        vs[i + 2 * p]   = (a0a1 - a2a3);
-        vs[i + 3 * p]   = (a0na1 - a2na3iimag);
+        modint a0         = vs[i + 0 * p];
+        modint a1         = vs[i + 1 * p];
+        modint a2         = vs[i + 2 * p];
+        modint a3         = vs[i + 3 * p];
+        modint a2na3iimag = (a2 - a3) * iimag;
+        modint a0na1      = a0 - a1;
+        modint a0a1       = a0 + a1;
+        modint a2a3       = a2 + a3;
+        vs[i + 0 * p]     = a0a1 + a2a3;
+        vs[i + 1 * p]     = (a0na1 + a2na3iimag);
+        vs[i + 2 * p]     = (a0a1 - a2a3);
+        vs[i + 3 * p]     = (a0na1 - a2na3iimag);
       }
-      mint irot = info.irate3[0];
+      modint irot = info.irate3[0];
       for (usize s: rep(1, 1 << (len - 2))) {
         usize offset = s << (h - len + 2);
-        mint irot2   = irot * irot;
-        mint irot3   = irot2 * irot;
+        modint irot2 = irot * irot;
+        modint irot3 = irot2 * irot;
         for (usize i: rep(0, p)) {
-          mint a0                = vs[i + offset + 0 * p];
-          mint a1                = vs[i + offset + 1 * p];
-          mint a2                = vs[i + offset + 2 * p];
-          mint a3                = vs[i + offset + 3 * p];
-          mint a2na3iimag        = (a2 - a3) * iimag;
-          mint a0na1             = a0 - a1;
-          mint a0a1              = a0 + a1;
-          mint a2a3              = a2 + a3;
+          modint a0              = vs[i + offset + 0 * p];
+          modint a1              = vs[i + offset + 1 * p];
+          modint a2              = vs[i + offset + 2 * p];
+          modint a3              = vs[i + offset + 3 * p];
+          modint a2na3iimag      = (a2 - a3) * iimag;
+          modint a0na1           = a0 - a1;
+          modint a0a1            = a0 + a1;
+          modint a2a3            = a2 + a3;
           vs[i + offset + 0 * p] = a0a1 + a2a3;
           vs[i + offset + 1 * p] = (a0na1 + a2na3iimag) * irot;
           vs[i + offset + 2 * p] = (a0a1 - a2a3) * irot2;
@@ -175,7 +175,7 @@ namespace luz::internal {
     if (len > 0) {
       usize p = 1 << (h - 1);
       for (usize i: rep(0, p)) {
-        mint ajp = vs[i] - vs[i + p];
+        modint ajp = vs[i] - vs[i + p];
         vs[i] += vs[i + p];
         vs[i + p] = ajp;
       }
