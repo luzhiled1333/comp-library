e8365a75201988e405c360d3e2bdce00f9700495
Luzhiled <Luzhiled+github@gmail.com>
2023-05-01 01:54:50 +0900

    add RSQ solver

---
+++ src/data-structure/segment-tree/presets/range-sum-query-solver.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "src/data-structure/segment-tree/point-mapping-range-fold-segment-tree.hpp"
+
+#include <algorithm>
+
+namespace luz::monoid {
+
+  template < typename T >
+  class RangeMaximumQueryMonoid {
+
+   public:
+    using value_type = T;
+
+    static constexpr T operation(T a, T b) {
+      return a + b;
+    }
+
+    static constexpr T identity() {
+      return T();
+    }
+  };
+
+} // namespace luz::monoid
+
+namespace luz {
+
+  template < typename T >
+  using RangeSumQuerySolvre =
+      SegmentTree< monoid::RangeSumQueryMonoid< T > >;
+
+} // namespace luz
