4495bca5c656b33b2487481639bb0a550ea9f443
Luzhiled <Luzhiled+github@gmail.com>
2023-05-01 01:50:36 +0900

    update dsl_2_a (to use rmq solver)

--- test/aoj/dsl_2_a.test.cpp
+++ test/aoj/dsl_2_a.test.cpp
@@ -2,35 +2,17 @@
 
 #include "src/cpp-template/header/rep.hpp"
 #include "src/cpp-template/header/type-alias.hpp"
-#include "src/data-structure/segment-tree/point-mapping-range-fold-segment-tree.hpp"
+#include "src/data-structure/segment-tree/presets/range-minimum-query-solver.hpp"
 
-#include <algorithm>
 #include <iostream>
-#include <limits>
 
 namespace luz {
-
-  template < typename T >
-  class Monoid {
-    static constexpr T identity_{std::numeric_limits< T >::max()};
-
-   public:
-    using value_type = T;
-
-    static constexpr T operation(T a, T b) {
-      return std::min(a, b);
-    }
-
-    static constexpr T identity() {
-      return identity_;
-    }
-  };
 
   void main_() {
     usize n, q;
     std::cin >> n >> q;
 
-    SegmentTree< Monoid< i32 > > seg(n);
+    RangeMinimumQuerySolver< i32 > seg(n);
     for ([[maybe_unused]] usize _: rep(0, q)) {
       usize com, x, y;
       std::cin >> com >> x >> y;
