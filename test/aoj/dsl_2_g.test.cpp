d34f49c842c4abdd1398e1e4747576e37b9abf8a
Luzhiled <Luzhiled+github@gmail.com>
2023-05-01 18:02:35 +0900

    add RAQ + RSQ solver

---
+++ test/aoj/dsl_2_g.test.cpp
@@ -0,0 +1,44 @@
+// verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/problems/DSL_2_G
+
+#include "src/cpp-template/header/rep.hpp"
+#include "src/cpp-template/header/type-alias.hpp"
+#include "src/data-structure/segment-tree/presets/range-add-range-sum-query-solver.hpp"
+
+#include <iostream>
+#include <limits>
+#include <vector>
+
+namespace luz {
+
+  void main_() {
+    usize n, q;
+    std::cin >> n >> q;
+
+    using Tree = RangeAddRangeSumQuerySolver< i64 >;
+    using F = Tree::value_type;
+    std::vector< F > init(n, {0, 1});
+    Tree seg(init);
+    for ([[maybe_unused]] usize _: rep(0, q)) {
+      usize com;
+      std::cin >> com;
+
+      if (not com) {
+        usize s, t;
+        i32 x;
+        std::cin >> s >> t >> x;
+
+        seg.apply(s - 1, t, x);
+      } else {
+        usize s, t;
+        std::cin >> s >> t;
+
+        std::cout << seg.fold(s - 1, t).value << std::endl;
+      }
+    }
+  }
+
+} // namespace luz
+
+int main() {
+  luz::main_();
+}
