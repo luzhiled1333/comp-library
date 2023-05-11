e9c187ca101a3cbdeee670981b08dd45e983d774
Luzhiled <Luzhiled+github@gmail.com>
2023-05-11 09:18:11 +0900

    make vector-2d

---
+++ src/geometry/2d/common/vector-2d.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "src/geometry/common/class/column-vector.hpp"
+
+namespace luz {
+
+  template < class T >
+  class Vector2d: public ColumnVector< 2, T > {
+    using c_vec = ColumnVector< 2, T >;
+    using internal_mat = internal::Mat< d, 1, T >;
+
+   public:
+    using internal::Mat< d, 1, T >::Mat;
+
+    Vector2d(const c_vec &vec): c_vec::ColumnVector(vec) {}
+    Vector(const internal_mat &mat): internal_mat::Mat(mat) {}
+
+    const T &x() const {
+      return this->as[0];
+    }
+    const T &y() const {
+      return this->as[1];
+    }
+
+    T &x() {
+      return this->as[0];
+    }
+    T &y() {
+      return this->as[1];
+    }
+  };
+
+} // namespace luz
--- src/geometry/common/class/column-vector.hpp
+++ src/geometry/common/class/column-vector.hpp
@@ -6,33 +6,13 @@
 namespace luz {
 
   template < usize d, class T >
-  class Vector: public internal::Mat< d, 1, T > {
+  class ColumnVector: public internal::Mat< d, 1, T > {
     using internal_mat = internal::Mat< d, 1, T >;
 
    public:
     using internal::Mat< d, 1, T >::Mat;
 
-    Vector(const internal_mat &mat): internal_mat::Mat(mat) {}
-
-    const T &x() const {
-      return this->as[0];
-    }
-    const T &y() const {
-      return this->as[1];
-    }
-    const T &z() const {
-      return this->as[2];
-    }
-
-    T &x() {
-      return this->as[0];
-    }
-    T &y() {
-      return this->as[1];
-    }
-    T &z() {
-      return this->as[2];
-    }
+    ColumnVector(const internal_mat &mat): internal_mat::Mat(mat) {}
 
     T norm() const {
       T result{};
