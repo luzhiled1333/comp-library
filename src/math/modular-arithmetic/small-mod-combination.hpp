0bb2dc23f215988785b04542d075e60b796640a0
Luzhiled <Luzhiled+github@gmail.com>
2023-05-19 15:22:19 +0900

    append small-mod-combination.hpp

---
+++ src/math/modular-arithmetic/small-mod-combination.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "src/math/modular-arithmetic/modular-combinatorics.hpp"
+
+namespace luz {
+
+  template< typename modint >
+  class SmallModCombination {
+    static constexpr u32 mod = modint::get_mod();
+    Combinatorics< modint > mc;
+
+   public:
+    SmallModCombination(): mc(mod - 1) {}
+
+    modint combination(usize n, usize k) {
+      modint result = 1;
+      while (n) {
+        result *= mc.combination(n % mod, k % mod);
+        n /= mod;
+        k /= mod;
+      }
+
+      return result;
+    }
+  };
+
+} // namespace luz
