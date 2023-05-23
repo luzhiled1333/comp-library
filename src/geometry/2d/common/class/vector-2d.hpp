80d61a37120b075cee0f164d37e161272bfaf53c
Luzhiled <Luzhiled+github@gmail.com>
2023-05-23 10:43:12 +0900

    fix common::vector2d

--- src/geometry/2d/common/class/vector-2d.hpp
+++ src/geometry/2d/common/class/vector-2d.hpp
@@ -2,18 +2,14 @@
 
 #include "src/geometry/common/class/column-vector.hpp"
 
-namespace luz {
+namespace luz::geometry::common {
 
   template < class T >
   class Vector2d: public ColumnVector< 2, T > {
     using c_vec        = ColumnVector< 2, T >;
-    using internal_mat = internal::Mat< d, 1, T >;
 
    public:
-    using internal::Mat< d, 1, T >::Mat;
-
     Vector2d(const c_vec &vec): c_vec::ColumnVector(vec) {}
-    Vector2d(const internal_mat &mat): internal_mat::Mat(mat) {}
 
     const T &x() const {
       return this->as[0];
