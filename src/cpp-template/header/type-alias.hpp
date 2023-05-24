b6d36a6a2cb20f0fd9d655dedb9b1532f7238d05
ei1333 <ei1333hobby@gmail.com>
2023-05-24 12:54:14 +0900

    add i128 / u128

--- src/cpp-template/header/type-alias.hpp
+++ src/cpp-template/header/type-alias.hpp
@@ -8,9 +8,11 @@ namespace luz {
   using isize = std::ptrdiff_t;
   using usize = std::size_t;
 
-  using i32 = std::int32_t;
-  using i64 = std::int64_t;
-  using u32 = std::uint32_t;
-  using u64 = std::uint64_t;
+  using i32  = std::int32_t;
+  using i64  = std::int64_t;
+  using i128 = __int128_t;
+  using u32  = std::uint32_t;
+  using u64  = std::uint64_t;
+  using u128 = __uint128_t;
 
 } // namespace luz
