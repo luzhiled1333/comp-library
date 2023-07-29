e9a537968e3740d8cf78c3aea9f4c29b8f634970
Luzhiled <Luzhiled+github@gmail.com>
2023-07-29 18:18:33 +0900

    split chmin, chmax

--- src/cpp-template/header/change-min.hpp
+++ src/cpp-template/header/change-min.hpp
@@ -3,11 +3,6 @@
 namespace luz {
 
   template < typename T1, typename T2 >
-  inline bool chmax(T1 &a, T2 b) {
-    return a < b and (a = b, true);
-  }
-
-  template < typename T1, typename T2 >
   inline bool chmin(T1 &a, T2 b) {
     return a > b and (a = b, true);
   }
