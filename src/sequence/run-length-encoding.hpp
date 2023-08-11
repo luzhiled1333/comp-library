a16e44561074db4dd1dba27ad9af1f87772a8f83
Luzhiled <Luzhiled+github@gmail.com>
2023-08-11 19:35:51 +0900

    Update run length encoding (#191)
    * append iterator-using method
    
    * delete internal method
    
    * update docs
    
    * apply clang-format

--- src/sequence/run-length-encoding.hpp
+++ src/sequence/run-length-encoding.hpp
@@ -6,35 +6,34 @@
 #include <utility>
 #include <vector>
 
-namespace luz::impl {
+namespace luz {
 
-  template < typename T >
-  std::vector< std::pair< T, usize > > rle(
-      const std::vector< T > &vs) {
+  template < class T, class Iter >
+  std::vector< std::pair< T, usize > > run_length_encoding(
+      Iter first, Iter last) {
     std::vector< std::pair< T, usize > > result;
-    for (const auto &v: vs) {
-      if (result.empty() or result.back().first != v) {
-        result.emplace_back(v, 0);
+
+    while (first != last) {
+      if (result.empty() or result.back().first != *first) {
+        result.emplace_back(*first, 0);
       }
+
       result.back().second++;
+      ++first;
     }
+
     return result;
   }
-
-} // namespace luz::impl
-
-namespace luz {
 
   template < typename T >
   std::vector< std::pair< T, usize > > run_length_encoding(
       const std::vector< T > &vs) {
-    return impl::rle(vs);
+    return run_length_encoding(vs.begin(), vs.end());
   }
 
   std::vector< std::pair< char, usize > > run_length_encoding(
       const std::string &s) {
-    std::vector< char > cs(s.begin(), s.end());
-    return impl::rle(cs);
+    return run_length_encoding< char >(s.begin(), s.end());
   }
 
 } // namespace luz
