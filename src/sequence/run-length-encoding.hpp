3534a6b50292e108544dabe9bcb6b4958950a034
Luzhiled <Luzhiled+github@gmail.com>
2023-08-11 19:26:07 +0900

    delete internal method

--- src/sequence/run-length-encoding.hpp
+++ src/sequence/run-length-encoding.hpp
@@ -6,23 +6,6 @@
 #include <utility>
 #include <vector>
 
-namespace luz::impl {
-
-  template < typename T >
-  std::vector< std::pair< T, usize > > rle(
-      const std::vector< T > &vs) {
-    std::vector< std::pair< T, usize > > result;
-    for (const auto &v: vs) {
-      if (result.empty() or result.back().first != v) {
-        result.emplace_back(v, 0);
-      }
-      result.back().second++;
-    }
-    return result;
-  }
-
-} // namespace luz::impl
-
 namespace luz {
 
   template< class T, class Iter >
