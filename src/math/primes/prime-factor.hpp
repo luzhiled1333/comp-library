4f246b9229f2469b184b391fba142a41045de550
Luzhiled <Luzhiled+github@gmail.com>
2023-08-14 20:13:33 +0900

    u32 -> u64

--- src/math/primes/prime-factor.hpp
+++ src/math/primes/prime-factor.hpp
@@ -6,11 +6,10 @@
 
 namespace luz {
 
-  std::vector< u32 > prime_factor(u32 n) {
-    assert(1 <= n and n <= 65535 * 65535);
-    std::vector< u32 > ps;
+  std::vector< u64 > prime_factor(u64 n) {
+    std::vector< u64 > ps;
 
-    for (u32 p = 2; p * p <= n and n != 1; p++) {
+    for (u64 p = 2; p * p <= n and n != 1; p++) {
       while (n % p == 0) {
         ps.emplace_back(p);
         n /= p;
